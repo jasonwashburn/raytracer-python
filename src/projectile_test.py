85a996250ceeb8640fa5aca6b39fca65441dc55d
Jason Washburn <jason.washburn@gmail.com>
2023-05-04 04:56:09 -0500

    feat: :hammer: updates projectile test to output ppm

--- src/projectile_test.py
+++ src/projectile_test.py
@@ -1,4 +1,5 @@
-from raytracer.tuples import TupleFeature, Vector, normalize, point, vector
+from raytracer.canvas import Canvas, canvas_to_ppm, write_pixel
+from raytracer.tuples import Color, TupleFeature, Vector, normalize, point, vector
 
 
 class Projectile:
@@ -23,11 +24,20 @@ if __name__ == "__main__":
     # projectile starts one unit above the origin.
     # velocity is normalized to 1 unit/tick.
     # p ← projectile(point(0, 1, 0), normalize(vector(1, 1, 0)))
-    projectile = Projectile(position=point(0, 1, 0), velocity=normalize(vector(1, 1, 0)))
+    projectile = Projectile(position=point(0, 1, 0), velocity=normalize(vector(1, 1.8, 0)) * 11.25)
     # # gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
     # e ← environment(vector(0, -0.1, 0), vector(-0.01, 0, 0))
     environment = Environment(gravity=vector(0, -0.1, 0), wind=vector(-0.01, 0, 0))
 
+    canvas = Canvas(900, 550)
+
     while projectile.position.y > 0:
+        x = projectile.position.x
+        y = canvas.height - projectile.position.y
+        write_pixel(canvas, int(x), int(y), Color(128, 128, 128))
         print(projectile.position)  # noqa
         projectile = tick(env=environment, proj=projectile)
+
+    ppm = canvas_to_ppm(canvas)
+    with open("projectile.ppm", "w") as f:
+        f.write(ppm)
